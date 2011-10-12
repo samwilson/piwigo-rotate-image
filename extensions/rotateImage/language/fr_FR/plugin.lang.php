474aaa695a17b7b752cf0f5b35879e3e4a5f0609
Dsls <Dsls@68402e56-0260-453c-a942-63ccdbb3a9ee>
2011-10-12 18:40:15 +0000

    rotateImage v0.3 : * thumbnails are correctly refreshed * strengthened ws * small tuning on form display * it is now possible to select whether HD should be rotated or not

--- extensions/rotateImage/language/fr_FR/plugin.lang.php
+++ extensions/rotateImage/language/fr_FR/plugin.lang.php
@@ -1,8 +1,9 @@
 <?php
 $lang['Images rotation in progress...'] = 'Rotation des images en cours...';
 $lang['Rotate images'] = 'Pivoter les images';
-$lang['Rotate 90° right'] = 'Rotation de 90° à droite';
-$lang['Rotate 90° left'] = 'Rotation de 90° à gauche';
-$lang['Rotate 180°'] = 'Rotation de 180°';
-
+$lang['Angle'] = 'Angle';
+$lang['Also rotate HD image'] = 'Pivoter aussi l\'image HD';
+$lang['90° right'] = '90° à droite';
+$lang['90° left'] = '90° à gauche';
+$lang['180°'] = '180°';
 ?>
