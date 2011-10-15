d197ba5e4644b62861b59ab31e466d89f5cde1e6
ddtddt <ddtddt@68402e56-0260-453c-a942-63ccdbb3a9ee>
2011-10-15 15:05:06 +0000

    [extensions] - rotateImage - Add en_UK -English

---
+++ extensions/rotateImage/language/en_UK/plugin.lang.php
@@ -0,0 +1,8 @@
+<?php
+$lang['Images rotation in progress...'] = 'Images rotation in progress...';
+$lang['Rotate images'] = 'Rotate images';
+$lang['Rotate 90° right'] = 'Rotate 90° right';
+$lang['Rotate 90° left'] = 'Rotate 90° left';
+$lang['Rotate 180°'] = 'Rotate 180°';
+
+?>
