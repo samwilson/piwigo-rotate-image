02e2a2f12fcc7a09c82aea305acf2e5ae6aa92fc
ddtddt <ddtddt@68402e56-0260-453c-a942-63ccdbb3a9ee>
2011-10-15 15:06:09 +0000

    [extensions] - rotateImage - sk_SK – Slovensky – Slovak - Thanks to dodo

---
+++ extensions/rotateImage/language/sk_SK/plugin.lang.php
@@ -0,0 +1,8 @@
+<?php
+$lang['Images rotation in progress...'] = 'Fotografie sa otáčajú...'; 
+$lang['Rotate images'] = 'Otočiť fotky'; 
+$lang['Rotate 90° right'] = 'Otočiť 90° doprava'; 
+$lang['Rotate 90° left'] = 'Otočiť 90° doľava'; 
+$lang['Rotate 180°'] = 'Otočiť o 180°';
+
+?>
