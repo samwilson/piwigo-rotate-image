18d82351089686ea9c851a7a49961464beaca53c
ddtddt <ddtddt@68402e56-0260-453c-a942-63ccdbb3a9ee>
2011-10-16 17:35:41 +0000

    [extensions] - rotateImage - add nl_NL - Nederlands - Dutch - Thanks to Kees Hessels

---
+++ language/nl_NL/plugin.lang.php
@@ -0,0 +1,8 @@
+<?php
+$lang['Images rotation in progress...'] = 'Foto\'s worden geroteerd (gedraaid)...';
+$lang['Rotate images'] = 'Roteer (draai) foto\'s';
+$lang['Rotate 90° right'] = 'Roteer (draai) 90° rechtsom';
+$lang['Rotate 90° left'] = 'Roteer (draai) 90° linksom';
+$lang['Rotate 180°'] = 'Roteer (draai) 180°';
+
+?>
