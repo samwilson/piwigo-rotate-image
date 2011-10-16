b3c986b1e41801b620d81cc246bb72e765dc1b16
ddtddt <ddtddt@68402e56-0260-453c-a942-63ccdbb3a9ee>
2011-10-16 17:29:53 +0000

    [extensions] - rotateImage - add pl_PL - Polski - Polish - Thanks to kuba

---
+++ language/pl_PL/plugin.lang.php
@@ -0,0 +1,8 @@
+<?php
+$lang['Images rotation in progress...'] = 'Trwa obracanie zdjęć...';
+$lang['Rotate images'] = 'Obróć zdjęcia';
+$lang['Rotate 90° right'] = 'Obróć o 90° w prawo';
+$lang['Rotate 90° left'] = 'Obróć o 90° w lewo';
+$lang['Rotate 180°'] = 'Obróć o 180°'; 
+
+?>
