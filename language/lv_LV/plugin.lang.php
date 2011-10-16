68db5a45d0fc424b2494472079ef7135b8365dff
ddtddt <ddtddt@68402e56-0260-453c-a942-63ccdbb3a9ee>
2011-10-16 18:34:17 +0000

    [extensions] - rotateImage - add lv_LV - Latviešu - Latvian - Thanks to Aivars Baldone

---
+++ language/lv_LV/plugin.lang.php
@@ -0,0 +1,8 @@
+<?php
+$lang['Images rotation in progress...'] = 'Attēla pagriešana (rotācija) procesā...';
+$lang['Rotate images'] = 'Veikt attēlu pagriešanu';
+$lang['Rotate 90° right'] = 'Veikt pagriešanu 90° pa labi';
+$lang['Rotate 90° left'] = 'Veikt pagriešanu 90° pa kreisi';
+$lang['Rotate 180°'] = 'Veikt pagriešanu pa 180°';
+
+?>
