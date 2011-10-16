f4dacf09b9016c4c45e350311d87f9f7399154e3
ddtddt <ddtddt@68402e56-0260-453c-a942-63ccdbb3a9ee>
2011-10-16 18:26:29 +0000

    [extensions] - rotateImage - add es_ES - Español – Spanish - Thanks to Kees jpr928

---
+++ extensions/rotateImage/language/es_ES/plugin.lang.php
@@ -0,0 +1,8 @@
+<?php
+$lang['Images rotation in progress...'] = 'Procesando rotación de imagenes...';
+$lang['Rotate images'] = 'Rotar las imágenes';
+$lang['Rotate 90° right'] = 'Girar 90 ° a la derecha';
+$lang['Rotate 90° left'] = 'Girar 90 ° a la izquierda';
+$lang['Rotate 180°'] = 'Rotación a 180°';
+
+?>
