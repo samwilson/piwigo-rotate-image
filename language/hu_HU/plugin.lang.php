c8f9c95a9e4f7017cebd4816c8ed199f17504850
ddtddt <ddtddt@68402e56-0260-453c-a942-63ccdbb3a9ee>
2011-10-16 17:24:10 +0000

    [extensions] - rotateImage - add hu_HU - Magyar – Hungarian - Thanks to evryka23

---
+++ language/hu_HU/plugin.lang.php
@@ -0,0 +1,8 @@
+<?php
+$lang['Images rotation in progress...'] = 'Képek forgatása folyamatban...';
+$lang['Rotate images'] = 'Képek forgatása';
+$lang['Rotate 90° right'] = 'Forgatás 90° jobb';
+$lang['Rotate 90° left'] = 'Forgatás 90° bal';
+$lang['Rotate 180°'] = 'Forgatás 180°';
+
+?>
