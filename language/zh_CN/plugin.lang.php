34c33313993ab07bc2f614166c95dc9ebdd956d2
TranslationTeam <TranslationTeam@68402e56-0260-453c-a942-63ccdbb3a9ee>
2012-01-17 15:23:34 +0000

    [rotateImage] Update zh_CN, thanks to : quqibing

--- language/zh_CN/plugin.lang.php
+++ language/zh_CN/plugin.lang.php
@@ -3,4 +3,9 @@
 $lang['Images rotation in progress...'] = '正在进行旋转相片处理...';
 $lang['Rotate images'] = '旋转相片';
 
+$lang['180°'] = '旋转180°';
+$lang['90° left'] = '左转90°';
+$lang['90° right'] = '右转90°';
+$lang['Also rotate HD image'] = '也旋转高清图像';
+$lang['Angle'] = '角度';
 ?>
