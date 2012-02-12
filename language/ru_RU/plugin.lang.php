cad1055cfac6d8bf92cac6e406a3b02fd72f5b76
ddtddt <ddtddt@68402e56-0260-453c-a942-63ccdbb3a9ee>
2012-02-12 12:14:38 +0000

    [extensions] - rotateImage - add ru_RU - Русский - Russian - Thanks to IE

---
+++ language/ru_RU/plugin.lang.php
@@ -0,0 +1,7 @@
+<?php
+$lang['Images rotation in progress...'] = 'Производится поворот изображений...';
+$lang['Rotate images'] = 'Повернуть изображения';
+$lang['Rotate 90° right'] = 'Повернуть на 90° вправо';
+$lang['Rotate 90° left'] = 'Повернуть на 90° влево';
+$lang['Rotate 180°'] = 'Повернуть на 180°';
+?>
