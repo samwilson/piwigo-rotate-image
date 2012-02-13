d24a7e09cb32d9825f67ac4c9e24c9389911fd56
duke <duke@68402e56-0260-453c-a942-63ccdbb3a9ee>
2012-02-13 08:30:04 +0000

    added de_DE

---
+++ language/de_DE/plugin.lang.php
@@ -0,0 +1,9 @@
+<?php
+$lang['Images rotation in progress...'] = 'Bilder werden gedreht...';
+$lang['Rotate images'] = 'Bilder drehen';
+$lang['Angle'] = 'Winkel';
+$lang['Also rotate HD image'] = 'Auch HD Bilder drehen';
+$lang['90° right'] = '90° rechts';
+$lang['90° left'] = '90° links';
+$lang['180°'] = '180°';
+?>
