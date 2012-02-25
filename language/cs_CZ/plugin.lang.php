dc466ea8a36b935ce8d69f32439295e5b66cc74f
ddtddt <ddtddt@68402e56-0260-453c-a942-63ccdbb3a9ee>
2012-02-25 17:08:11 +0000

    [extensions] - rotateImage - add cs_CZ - Česky – Czech - Thanks to puschpull

---
+++ language/cs_CZ/plugin.lang.php
@@ -0,0 +1,10 @@
+<?php
+$lang['Images rotation in progress...'] = 'Fotografie sa otáčejí...'; 
+$lang['Rotate images'] = 'Otočit fotky'; 
+$lang['Angle'] = 'Úhel'; 
+$lang['Also rotate HD image'] = 'Otočit také HD obrázek'; 
+$lang['90° right'] = '90° doprava'; 
+$lang['90° left'] = '90° doleva'; 
+$lang['180°'] = 'O 180°'; 
+$lang['auto (EXIF orientation tag)'] = 'automaticky (podle EXIF informace /orientation tag/)';
+?>
