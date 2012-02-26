2d76a34dd4791a1eed119fc7afb02e990051e06f
ddtddt <ddtddt@68402e56-0260-453c-a942-63ccdbb3a9ee>
2012-02-26 20:19:40 +0000

    [extensions] - rotateImage - update ru_RU - Русский - Russian - Thanks to IE

--- extensions/rotateImage/language/ru_RU/plugin.lang.php
+++ extensions/rotateImage/language/ru_RU/plugin.lang.php
@@ -1,7 +1,11 @@
 <?php
 $lang['Images rotation in progress...'] = 'Производится поворот изображений...';
 $lang['Rotate images'] = 'Повернуть изображения';
-$lang['Rotate 90° right'] = 'Повернуть на 90° вправо';
-$lang['Rotate 90° left'] = 'Повернуть на 90° влево';
-$lang['Rotate 180°'] = 'Повернуть на 180°';
+$lang['Angle'] = 'Угол';
+$lang['Also rotate HD image'] = 'Также повернуть HD-изображения';
+$lang['90° right'] = '90° вправо';
+$lang['90° left'] = '90° влево';
+$lang['180°'] = '180°';
+$lang['auto (EXIF orientation tag)'] = 'автоматический поворот (по EXIF-тэгу в файле снимка)';
+
 ?>
