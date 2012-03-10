303da3a24522f218bd1f15cc18057c2167a02aad
ddtddt <ddtddt@68402e56-0260-453c-a942-63ccdbb3a9ee>
2012-03-10 06:54:07 +0000

    [extensions] - rotateImage - add vi_VN - Tiếng Việt - Vietnamese - Thanks to Ici

---
+++ extensions/rotateImage/language/vi_VN/plugin.lang.php
@@ -0,0 +1,10 @@
+<?php
+$lang['Images rotation in progress...'] = 'Đang xoay hình...';
+$lang['Rotate images'] = 'Xoay hình';
+$lang['Angle'] = 'Góc xoay';
+$lang['Also rotate HD image'] = 'Xoay cả phiên bản phân giải cao';
+$lang['90° right'] = '90 độ về bên phải';
+$lang['90° left'] = '90 độ về bên trái';
+$lang['180°'] = '180 độ';
+$lang['auto (EXIF orientation tag)'] = 'tự động (theo thông tin EXIF)';
+?>
