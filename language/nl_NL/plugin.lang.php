a10dac64073e9fe878042ea11e435c278f6df664
TranslationTeam <TranslationTeam@68402e56-0260-453c-a942-63ccdbb3a9ee>
2012-03-17 11:28:51 +0000

    [rotateImage] Update nl_NL, thanks to : Kees Hessels

--- language/nl_NL/plugin.lang.php
+++ language/nl_NL/plugin.lang.php
@@ -7,4 +7,5 @@ $lang['90° left'] = '90° linksom';
 $lang['90° right'] = '90° rechtsom';
 $lang['Also rotate HD image'] = 'Roteer ook HD afbeeldingen';
 $lang['Angle'] = 'Hoek';
+$lang['auto (EXIF orientation tag)'] = 'automatisch (EXIF oriëntatie-label)';
 ?>
