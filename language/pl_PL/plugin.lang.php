4cefaec782e74efb9ad872fe6abe50023e8f2721
TranslationTeam <TranslationTeam@68402e56-0260-453c-a942-63ccdbb3a9ee>
2012-03-17 11:28:34 +0000

    [rotateImage] Update pl_PL, thanks to : Greybrow

--- language/pl_PL/plugin.lang.php
+++ language/pl_PL/plugin.lang.php
@@ -1,8 +1,11 @@
 <?php
 $lang['Images rotation in progress...'] = 'Trwa obracanie zdjęć...';
 $lang['Rotate images'] = 'Obróć zdjęcia';
-$lang['Rotate 90° right'] = 'Obróć o 90° w prawo';
-$lang['Rotate 90° left'] = 'Obróć o 90° w lewo';
-$lang['Rotate 180°'] = 'Obróć o 180°'; 
 
+$lang['180°'] = '180°';
+$lang['90° left'] = '90° w lewo';
+$lang['90° right'] = '90° w prawo';
+$lang['Also rotate HD image'] = 'Obróć również obraz wysokiej jakości HD';
+$lang['Angle'] = 'Kąt';
+$lang['auto (EXIF orientation tag)'] = 'automatycznie (wg orientacji w EXIF)';
 ?>
