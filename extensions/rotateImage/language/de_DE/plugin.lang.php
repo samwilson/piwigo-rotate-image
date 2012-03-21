4fe50e68a709ddd2565d813b8346f9e44d556a83
TranslationTeam <TranslationTeam@68402e56-0260-453c-a942-63ccdbb3a9ee>
2012-03-21 20:47:33 +0000

    [Rotate Image] Update de_DE, thanks to : duke

--- extensions/rotateImage/language/de_DE/plugin.lang.php
+++ extensions/rotateImage/language/de_DE/plugin.lang.php
@@ -6,4 +6,5 @@ $lang['Also rotate HD image'] = 'Auch HD Bilder drehen';
 $lang['90° right'] = '90° rechts';
 $lang['90° left'] = '90° links';
 $lang['180°'] = '180°';
+$lang['auto (EXIF orientation tag)'] = 'automatisch (wertet EXIF-Ausrichtung aus)';
 ?>
