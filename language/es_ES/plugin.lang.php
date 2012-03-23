fb79a0715c4e5475ed1326c70b7683fb17e18e36
TranslationTeam <TranslationTeam@68402e56-0260-453c-a942-63ccdbb3a9ee>
2012-03-23 12:37:10 +0000

    [Rotate Image] Update es_ES, thanks to : petaqui

--- language/es_ES/plugin.lang.php
+++ language/es_ES/plugin.lang.php
@@ -1,8 +1,11 @@
 <?php
 $lang['Images rotation in progress...'] = 'Procesando rotación de imagenes...';
 $lang['Rotate images'] = 'Rotar las imágenes';
-$lang['Rotate 90° right'] = 'Girar 90 ° a la derecha';
-$lang['Rotate 90° left'] = 'Girar 90 ° a la izquierda';
-$lang['Rotate 180°'] = 'Rotación a 180°';
 
+$lang['180°'] = '180º';
+$lang['90° left'] = '90º a la izquierda';
+$lang['90° right'] = '90º a la derecha';
+$lang['Also rotate HD image'] = 'Rotar también la imagen en HD';
+$lang['Angle'] = 'Ángulo';
+$lang['auto (EXIF orientation tag)'] = 'auto (Basado en los datos EXIF de orientación)';
 ?>
