f2e65fa1365691ee0d29058c7c2b80e9dd5f903a
TranslationTeam <TranslationTeam@68402e56-0260-453c-a942-63ccdbb3a9ee>
2012-05-23 21:14:05 +0000

    [rotateimage] Update ca_ES, thanks to : AlbertParera

---
+++ extensions/rotateImage/language/ca_ES/plugin.lang.php
@@ -0,0 +1,31 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['180°'] = '180º';
+$lang['90° left'] = '90º a l\'esquerra';
+$lang['90° right'] = '90º a la dreta';
+$lang['Also rotate HD image'] = 'Rotar també imatges en alta qualitat (HD)';
+$lang['Angle'] = 'Angle';
+$lang['Images rotation in progress...'] = 'Rotació d\'imatges en curs...';
+$lang['Rotate images'] = 'Rotar imatges';
+$lang['auto (EXIF orientation tag)'] = 'Automàtic (etiqueta d\'orientació Exif)';
+?>
