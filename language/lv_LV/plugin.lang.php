1cfe3e844fa59ce9411cfe0c103d2803447223f9
TranslationTeam <TranslationTeam@68402e56-0260-453c-a942-63ccdbb3a9ee>
2012-06-16 17:01:14 +0000

    [rotateimage] Update lv_LV, thanks to : agrisans

--- language/lv_LV/plugin.lang.php
+++ language/lv_LV/plugin.lang.php
@@ -1,8 +1,11 @@
 <?php
 $lang['Images rotation in progress...'] = 'Attēla pagriešana (rotācija) procesā...';
 $lang['Rotate images'] = 'Veikt attēlu pagriešanu';
-$lang['Rotate 90° right'] = 'Veikt pagriešanu 90° pa labi';
-$lang['Rotate 90° left'] = 'Veikt pagriešanu 90° pa kreisi';
-$lang['Rotate 180°'] = 'Veikt pagriešanu pa 180°';
 
+$lang['180°'] = '180°';
+$lang['90° left'] = '90° pa kreisi';
+$lang['90° right'] = '90° pa labi';
+$lang['Also rotate HD image'] = 'Rotēt arī HD attēlu';
+$lang['Angle'] = 'Leņķis';
+$lang['auto (EXIF orientation tag)'] = 'auto(EXIF orientācijas tegs)';
 ?>
