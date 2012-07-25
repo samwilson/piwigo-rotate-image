9fb5fb2b6e24cadc3a5e424ec3b2609b450e4b1e
TranslationTeam <TranslationTeam@68402e56-0260-453c-a942-63ccdbb3a9ee>
2012-07-25 17:59:46 +0000

    [rotateimage] Update sk_SK, thanks to : dodo

--- extensions/rotateImage/language/sk_SK/plugin.lang.php
+++ extensions/rotateImage/language/sk_SK/plugin.lang.php
@@ -8,4 +8,5 @@ $lang['90° right'] = '90° vpravo';
 $lang['Also rotate HD image'] = 'Otočiť aj HD fotku';
 $lang['Angle'] = 'Uhol';
 $lang['auto (EXIF orientation tag)'] = 'automaticky (podľa EXIF)';
+$lang['Rotate'] = 'Otočiť';
 ?>
