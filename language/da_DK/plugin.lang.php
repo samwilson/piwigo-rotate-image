98ac98c4296c4261acffef7c94903080b6b42a01
TranslationTeam <TranslationTeam@68402e56-0260-453c-a942-63ccdbb3a9ee>
2012-09-25 21:11:30 +0000

    [rotateimage] Update da_DK, thanks to : Kaare

---
+++ language/da_DK/plugin.lang.php
@@ -0,0 +1,32 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['180°'] = '180°';
+$lang['90° left'] = '90° mod uret';
+$lang['90° right'] = '90° med uret';
+$lang['Also rotate HD image'] = 'Rotér også HD-billede';
+$lang['Angle'] = 'Vinkel';
+$lang['Images rotation in progress...'] = 'I færd med at rotere billeder ...';
+$lang['Rotate'] = 'Rotér';
+$lang['Rotate images'] = 'Rotér billeder';
+$lang['auto (EXIF orientation tag)'] = 'auto (retningstag fra EXIF)';
+?>
