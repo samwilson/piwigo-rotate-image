6cfb7132835a3363c6441a86bf530485975c7b2e
TranslationTeam <TranslationTeam@68402e56-0260-453c-a942-63ccdbb3a9ee>
2012-10-08 16:09:30 +0000

    [rotateimage] Update zh_CN, thanks to : foddyy

--- language/zh_CN/plugin.lang.php
+++ language/zh_CN/plugin.lang.php
@@ -9,4 +9,5 @@ $lang['90° right'] = '右转90°';
 $lang['Also rotate HD image'] = '也旋转高清图像';
 $lang['Angle'] = '角度';
 $lang['auto (EXIF orientation tag)'] = '自动旋转(根据EXIF的方向标签)';
+$lang['Rotate'] = '旋转';
 ?>
