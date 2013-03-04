cdd2389b7e9f13538ce1a415938c7fce8b91e0af
TranslationTeam <TranslationTeam@68402e56-0260-453c-a942-63ccdbb3a9ee>
2013-03-04 07:30:24 +0000

    [rotateimage] Add bg_BG, thanks to : picsvet

---
+++ language/bg_BG/plugin.lang.php
@@ -0,0 +1,32 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['auto (EXIF orientation tag)'] = 'Auto (EXIF ориентация таг)';
+$lang['Rotate images'] = 'Завъртане на изображението';
+$lang['Rotate'] = 'Завъртете';
+$lang['Images rotation in progress...'] = 'Снимки на въртене е в ход ...';
+$lang['Angle'] = 'Ъгъл';
+$lang['Also rotate HD image'] = 'Също да завъртите HD изображение';
+$lang['90° right'] = '90 ° по посока на часовниковата стрелка';
+$lang['90° left'] = '90 ° обратно на часовниковата стрелка';
+$lang['180°'] = '180°';
+?>
