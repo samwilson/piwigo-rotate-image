c7af7d7ca88d2425dd3c905c3638b5044908f76a
TranslationTeam <TranslationTeam@68402e56-0260-453c-a942-63ccdbb3a9ee>
2013-03-17 08:58:06 +0000

    [rotateimage] Add zh_TW, thanks to : tedx

---
+++ extensions/rotateImage/language/zh_TW/plugin.lang.php
@@ -0,0 +1,32 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['180°'] = '180度';
+$lang['90° left'] = '90度逆時針方向';
+$lang['90° right'] = '90度順時針方向';
+$lang['Also rotate HD image'] = '還可以旋轉高清圖像';
+$lang['Angle'] = '角度';
+$lang['Images rotation in progress...'] = '圖片旋轉中...';
+$lang['Rotate'] = '旋轉';
+$lang['Rotate images'] = '旋轉圖片';
+$lang['auto (EXIF orientation tag)'] = '自動(EXIF方向標記)';
+?>
