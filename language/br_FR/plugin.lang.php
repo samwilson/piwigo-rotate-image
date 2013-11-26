0201b6b8c85250ad027dff0359150c234333f3dd
TranslationTeam <TranslationTeam@68402e56-0260-453c-a942-63ccdbb3a9ee>
2013-11-26 20:03:26 +0000

    [rotateimage] Update br_FR, thanks to : yves.kerleguer

---
+++ language/br_FR/plugin.lang.php
@@ -0,0 +1,32 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['180°'] = '180°';
+$lang['90° left'] = '90° a-du gant nadozioù un eurier';
+$lang['90° right'] = '90° en enep da nadozioù un eurier';
+$lang['Also rotate HD image'] = 'Treiñ ar skeudenn uhel e zespizadur ivez';
+$lang['Angle'] = 'Korn';
+$lang['Images rotation in progress...'] = 'O treiñ ar skeudennoù…';
+$lang['Rotate'] = 'Treiñ';
+$lang['Rotate images'] = 'Treiñ ar skeudennoù';
+$lang['auto (EXIF orientation tag)'] = 'emgefreek (merker furmad EXIF)';
+?>
