727b91453d183f50ae3d464075ceb3a626f89006
TranslationTeam <TranslationTeam@68402e56-0260-453c-a942-63ccdbb3a9ee>
2014-01-09 20:51:30 +0000

    [rotateimage] Update fa_IR, thanks to : ipic

---
+++ extensions/rotateImage/language/fa_IR/plugin.lang.php
@@ -0,0 +1,33 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2014 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['180°'] = '180°';
+$lang['90° left'] = 'حرکت عقربه های ساعت °90';
+$lang['90° right'] = '
+در جهت ساعت';
+$lang['Also rotate HD image'] = ' HDهمچنین چرخاندن تصویر';
+$lang['Angle'] = 'گوشه';
+$lang['Images rotation in progress...'] = 'پیشرفت چرخش تصاویر   ...';
+$lang['Rotate'] = 'چرخش';
+$lang['Rotate images'] = 'چرخش تصویر';
+$lang['auto (EXIF orientation tag)'] = 'خودکار (EXIF orientation tag)';
+?>
