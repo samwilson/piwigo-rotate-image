f757d95911dc9ae433d2fe45c63b03346935e5b5
TranslationTeam <TranslationTeam@68402e56-0260-453c-a942-63ccdbb3a9ee>
2015-06-28 10:31:16 +0000

    Add he_IL, thanks to ymoran00

---
+++ extensions/rotateImage/language/he_IL/plugin.lang.php
@@ -0,0 +1,31 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2015 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['180°'] = '180°';
+$lang['90° left'] = '90° נגד כיוון השעון';
+$lang['90° right'] = '90° בכיוון השעון';
+$lang['Also rotate HD image'] = 'סובב גם תמונת HD';
+$lang['Angle'] = 'זווית';
+$lang['Images rotation in progress...'] = 'סיבוב תמונות מתבצע...';
+$lang['Rotate'] = 'סובב';
+$lang['Rotate images'] = 'סובב תמונות';
+$lang['auto (EXIF orientation tag)'] = 'אוטומטי (תווית כיוון ב-EXIF)';
