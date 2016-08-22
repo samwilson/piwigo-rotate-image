8fe4bf75aae49ba1360da82923fde6a6ef9f64a7
TranslationTeam <TranslationTeam@68402e56-0260-453c-a942-63ccdbb3a9ee>
2016-08-22 04:15:06 +0000

    Update nl_NL, thanks to watertoren5044

--- language/nl_NL/plugin.lang.php
+++ language/nl_NL/plugin.lang.php
@@ -5,8 +5,7 @@ $lang['Rotate images'] = 'Roteer (draai) foto\'s';
 $lang['180°'] = '180°';
 $lang['90° left'] = '90° linksom';
 $lang['90° right'] = '90° rechtsom';
-$lang['Also rotate HD image'] = 'Roteer ook HD afbeeldingen';
+$lang['Also rotate HD image'] = 'Roteer (draai) ook HD foto';
 $lang['Angle'] = 'Hoek';
 $lang['auto (EXIF orientation tag)'] = 'automatisch (EXIF oriëntatie-label)';
-$lang['Rotate'] = 'Roteer';
-?>
+$lang['Rotate'] = 'Roteer (draai)';
